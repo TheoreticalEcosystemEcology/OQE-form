3b5128229c1a8d537c7ef6ce4cb0ad8e7bce00ba
Eltisho <vincent.bellavance@USherbrooke.ca>
2017-01-26 11:42:00 -0500

    init repo

---
+++ server.R
@@ -0,0 +1,224 @@
+library("shiny")
+library("leaflet")
+library("leaflet.extras")
+
+
+# add data in the list
+shinyServer(function(input, output, session) {
+
+    source("./opForm.R", local = TRUE)
+
+
+    ######### GEO DATA #########
+
+    # Créé responses s'il n'existe pas
+    rec <- reactiveValues(map=list(),form=df)
+
+    if(!exists("responses")) responses <<- list()
+
+    output$map <- renderLeaflet({
+        leaflet() %>% addProviderTiles("Esri.WorldTopoMap") %>% setView(1010, 54, 5) %>%
+        addDrawToolbar(targetGroup=rv$page,polygonOptions = drawPolygonOptions(),
+            markerOptions = drawMarkerOptions(), editOptions = editToolbarOptions(),
+            polylineOptions = FALSE, circleOptions = FALSE, rectangleOptions = FALSE)
+    })
+
+    observeEvent(input$map_draw_new_feature,{
+      rec$map[[length(rec$map)+1]] <- input$map_draw_new_feature
+    })
+
+    observeEvent(input$map_draw_edited_features, {
+
+      id_edit <- input$map_draw_edited_features$features[[1]]$properties$`_leaflet_id`
+
+      for(i in 1:length(rec$map)){
+        if(rec$map[[i]]$properties$`_leaflet_id` == id_edit ){
+          rec$map[[i]] <- input$map_draw_edited_features
+        }
+      }
+
+    })
+
+    observeEvent(input$map_draw_deleted_features, {
+
+      id_edit <- input$map_draw_edited_features$features[[1]]$properties$`_leaflet_id`
+
+      for(i in 1:length(rep$map)){
+        if(rec$map[[i]]$properties$`_leaflet_id` == id_edit ) rec$map[[i]] <- input$map_draw_edited_features
+      }
+
+    })
+
+
+    ######### FORM DATA #########
+
+    # Whenever a field is filled, aggregate all form data
+    response <- reactive({
+
+        # Réponses aux questions
+        rec$form[1, 1] <- input$sample
+        rec$form[1, 2] <- as.character(input$yearRange)[1]
+        rec$form[1, 3] <- as.character(input$yearRange)[2]
+        rec$form[1, 4] <- input$sampleNo
+        rec$form[1, 5] <- as.character(input$year)
+        rec$form[1, 6] <- input$context
+        rec$form[1, 7:19] <- taxa_ref %in% input$taxa
+        rec$form[1, 20:23] <- type_ref %in% input$type
+        rec$form[1, 24:26] <- enviro_ref %in% input$enviro
+        rec$form[1, 27] <- input$share
+        rec$form[1, 28:38] <- db_ref %in% input$db
+
+        # Liste des réponses
+        response <- list(form=rec$form,map=rec$map)
+        response
+    })
+
+    # ########### PAGES BEHAVIOUR (PREVIOUS AND NEXT) #########
+    #
+    # # declare page var as reactive La page commence à 1 (Correspond à la page sur
+    # # lequel nous nous trouvons présentement. Donc au départ, on est sur la 1Ã¨re page
+    # # que nous allons remplir)
+
+    rv <- reactiveValues(page=1)
+
+    # # Disable/enable prev and observe page
+    observe({
+      toggleState(id = "prev", condition = rv$page > 1)
+      toggleState(id = "nxt", condition = rv$page <= length(responses))
+      toggleState(id="erase",condition = rv$page <= length(responses))
+    })
+
+    ########### PREVIOUS prev button behaviour
+    observeEvent(input$prev, {
+
+      # On change la page
+      rv$page <- rv$page - 1
+
+      # On met à jour les données
+      rec$map <- responses[[rv$page]]$map
+      rec$form <- responses[[rv$page]]$form
+
+      # On update le form
+      updateForm(rec$form)
+
+      # On met à jour la carte
+      leafletProxy("map") %>%
+      addGeoJSON(rec$map) %>%
+      addDrawToolbar(polygonOptions =  FALSE,
+          markerOptions = FALSE, editOptions = FALSE,
+          polylineOptions = FALSE, circleOptions = FALSE, rectangleOptions = FALSE)
+
+      # On update responses
+      responses[[rv$page]] <<- response()
+
+    })
+
+
+    ######### NEXT button behaviour
+    observeEvent(input$nxt, {
+
+      if(rv$page < length(responses)){
+
+        # On change la page
+        rv$page <- rv$page + 1
+
+        # On met à jour les données
+        rec$map <- responses[[rv$page]]$map
+        rec$form <- responses[[rv$page]]$form
+
+        # On update le form
+        updateForm(rec$form)
+
+        # On met à jour la carte
+        leafletProxy("map") %>%
+        addGeoJSON(rec$map) %>%
+        addDrawToolbar(polygonOptions =  FALSE,
+            markerOptions = FALSE, editOptions = FALSE,
+            polylineOptions = FALSE, circleOptions = FALSE, rectangleOptions = FALSE)
+
+        # On update responses
+        responses[[rv$page]] <<- response()
+
+      } else if (rv$page == length(responses)) {
+
+        rv$page <- rv$page + 1
+
+        # On réinitialise le formulaire à blanc
+        reinit(rec)
+
+      }
+
+    })
+    #
+    # ######### ADD button behaviour
+    #
+    observeEvent(input$add, {
+
+    # s'il n'y a pas d'enregistrements
+      if (length(responses) == 0) {
+        responses[[1]] <<- response()
+      } else {
+        # On ajoute les données
+        responses[[rv$page]] <<- response()
+
+      }
+
+      # On ajoute +1 au compteur de page
+      rv$page <- rv$page + 1
+
+      # On réinitialise le formulaire à blanc
+      reinit(rec)
+
+    })
+
+    # ########### ERASE FORM
+    observeEvent(input$erase, {
+      if(rv$page>1){
+        responses[[rv$page]] <<- NULL
+        rv$page <- rv$page - 1
+
+        # On met à jour les données
+        rec$map <- responses[[rv$page]]$map
+        rec$form <- responses[[rv$page]]$form
+
+        # On update le form
+        updateForm(rec$form)
+      } else {
+
+        responses[[rv$page]] <<- NULL
+        reinit(rec)
+
+        # On reinit la map comme rv$page change pas
+        output$map <- renderLeaflet({
+            leaflet() %>% addProviderTiles("Esri.WorldTopoMap") %>% setView(1010, 54, 5) %>%
+            addDrawToolbar(targetGroup=rv$page,polygonOptions = drawPolygonOptions(),
+                markerOptions = drawMarkerOptions(), editOptions = editToolbarOptions(),
+                polylineOptions = FALSE, circleOptions = FALSE, rectangleOptions = FALSE)
+        })
+
+      }
+    })
+
+    output$nCamp <- renderUI({
+      HTML(paste("<p style='font-size:13px;margin-top:10px'> Le nombre de campagnes d'échantillonage enregistrées est de: <b>",length(responses),"</b> </p>"))
+    })
+
+    # ########### SUBMIT FORM submit form
+    observeEvent(input$submit, {
+
+      # On ajoute les données
+      responses[[rv$page]] <<- response()
+
+      # On sauvegarde
+      saveRDS(responses,file=paste0("./data/",format(Sys.time(), "%Y%m%d_%H%M%S_"), "data_set.rds"))
+
+
+      ## Déclenche Modal pour demander les infos de la personne si elle a coché qu'elle était interessé à partager les données
+
+      ## Déclenche Modal pour dire que l'info a bien été enregistré.
+
+
+
+    })
+
+})
---
+++ ui.R
@@ -0,0 +1,101 @@
+library("shiny")
+library("leaflet")
+library("shinyjs")
+library("shinythemes")
+
+
+shinyUI(fluidPage(theme = shinytheme("yeti"),
+  includeCSS("stylesheet.css"),
+  sidebarLayout(position = "right",
+    sidebarPanel(style = "overflow-y:scroll; max-height: 100vh !important", useShinyjs(),
+    h3("Sondage sur vos campagnes d'échantillonage",align = "center",style="font-weight:bold;margin-bottom:15px"),
+    HTML("<hr width='75%'>"),
+    div(align="center",
+    actionButton("prev", label = "Précédente"),
+    actionButton("nxt", label = "Suivante"),
+    actionButton("erase", class = "btn-danger", label = "Effacer cette campagne")),
+        HTML("<hr width='75%'>"),
+        h4("Étape 1: Localisation de la campagne d'échantillonage"),
+        p("Veuillez ajouter la localisation géographique de votre campagne en utilisant la carte à gauche (le menu permet de dessiner des polygones et des points).",style="font-style:italic;font-size:13px;"),
+        HTML("<hr width='75%'>"),
+        h4("Étape 2: Formulaire"),
+        p("Veuillez compléter les informations relatives à la campagne d'échantillonage que vous venez de localiser sur la carte.",style="font-style:italic;font-size:13px;"),
+              div(
+                   id = "formulaire",
+                    # Campagne d'échantillonnage
+                    selectInput("sample", label =  h5("Campagne d'échantillonnage:"),
+                              choices = list("Ponctuelle" = 1, "Récurrente" = 2)),
+
+                    # Année d'échantillonnage (dateRange or date)
+                    conditionalPanel(
+                      condition = "input.sample == 2",
+                      dateRangeInput("yearRange",label = h5("Première et dernière année:"))
+                  ),
+                  conditionalPanel(
+                    condition = "input.sample == 2",
+                    numericInput("sampleNo",label = h5("Nombre d'échantillons durant cette période?"),
+                                 value = 1)
+                  ),
+                  conditionalPanel(
+                    condition = "input.sample == 1",
+                    dateInput("year", label = h5("Année:"))
+                  ),
+
+                  # Type d'échantillonnage
+                  radioButtons("context", label = h5("Type d'échantillonnage:"),
+                               choices = list("Observations" = 1, "Données expérimentales" = 2)),
+
+                  # Groupes taxonomiques étudiés
+                  checkboxGroupInput("taxa", label = h5("Groupe taxonomique:"),
+                                     choices = list("Mammifères" = "mamm", "Mammifères marins" ="mamm_marins", "Oiseaux" = "oiseaux",
+                                                    "Amphibiens" = "amph", "Reptiles" = "rept", "Poissons" = "poiss", "Arthropodes" = "arthr",
+                                                    "Invertébrés autre qu'arthropodes" = "autre_arthr", "Plantes vasculaires" = "plantes_vasc",
+                                                    "Plantes non-vasculaires" = "plantes_nonvasc", "Champignons, moisissures, levures" = "champ",
+                                                    "Organismes unicellulaires" = "unicell", "Bactéries" = "bact"), inline=TRUE),
+
+                  # Type de données
+                  checkboxGroupInput("type", label = h5("Type de données:"),
+                                     choices = list("Occurence" = "occur", "Abondance/Frequence" = "abond",
+                                                    "Données individuelles (Traits, Génétiques)" = "individu", "Autres" = "autres"),inline=TRUE),
+                  conditionalPanel(
+                    condition = "$.inArray('autres', input.type) > -1",
+                    textInput("autres_spec", label = h5("Vous avez séléctionné 'Autres', pourriez-vous préciser?"))
+                  ),
+
+                  # Environnement du/des site(s)
+                  checkboxGroupInput("enviro", label = h5("Type d'écosystème:"),
+                                     choices = list("Aquatique" = "aqua", "Marin" = "marin", "Terrestre" = "terre"),inline=TRUE),
+
+                  # Versées vers autres bases?
+                  radioButtons("share", label = h5("Est-ce que les données de cette campagne ont déjà été versées vers d'autres bases de données ouvertes?"),
+                               choices = list("Oui" = 1, "Non" = 2), selected = 2,inline=TRUE),
+                  conditionalPanel(
+                    condition = "input.share == 1",
+                    checkboxGroupInput("db", label = h5("Si oui, le(s)quel(s)?"),
+                                       choices = list("Canadensis/GBIF" = "gbif", "DataONE" = "dataone", "Dryad" = "dryad", "Encyclopedia of Life (EOL)" = "eol",
+                                                      "(ESA) data.esa.org" = "esa", "Fig Share" = "fig", "Mangal" = "mangal", "Le Naturaliste" = "naturaliste",
+                                                      "Nordicana D" = "nordi", "Quebio" = "quebio", "Autres" = "autre"),inline=TRUE)),
+
+                  conditionalPanel(
+                    condition = "$.inArray('autres', input.db) > -1",
+                    textInput("autres_spec", label = h5("Vous avez séléctionné 'Autres', pourriez-vous préciser?"))
+                  ),
+
+                  textAreaInput("comments",label= h5("Des commentaires/informations supplémentaires sur cette campagne?"),rows = 3)
+                  ),
+    HTML("<hr width='75%'>"),
+    h4("Étape 3: Ajouter une nouvelle campagne"),
+    p("Pour ajouter une nouvelle campagne d'échantillonnage, cliquez sur le bouton « Nouvelle campagne ».",style="font-style:italic;font-size:13px;"),
+    actionButton("add", class = "btn-primary", label = "Nouvelle campagne"),
+    uiOutput("nCamp"),
+    p("Utiliser les boutons 'suivant' et 'précédent' (en entête) pour les consulter.",style="font-style:italic;font-size:13px;"),
+    HTML("<hr width='75%'>"),
+    h4("Étape 4: Soumettre le formulaire"),
+    p("Lorsque vous avez ajoutez toutes vos campagnes, veuillez soumettre votre formulaire à l'aide du bouton « Soumettre le formulaire »",style="font-style:italic;font-size:13px;"),
+    actionButton("submit", class = "btn-success", label = "Soumettre")),
+
+    mainPanel(
+      leafletOutput("map")
+      )
+    )
+))
